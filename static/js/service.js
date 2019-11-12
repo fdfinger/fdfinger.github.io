2a8ae5030ba49ddaeb1bbe8ae2d75bd60f59c958
Jiangfei <jiangfei.ng@foxmail.com>
2019-11-12 14:40:24 +0800

    tijiao

--- static/js/service.js
+++ static/js/service.js
@@ -1,7 +1,7 @@
 const qs = {
   stringify: function (params) {
     let _form = new FormData()
-    for(let i of Object.keys(params)) {
+    for (let i of Object.keys(params)) {
       _form.append(i.toString(), params[i])
     }
     return _form
@@ -9,7 +9,7 @@ const qs = {
   url: function (url, params) {
     console.log(url, params);
     if (!params) return url
-    const _encode = Object.keys(params).map(function(item){
+    const _encode = Object.keys(params).map(function (item) {
       return `${item}=${params[item]}`
     }).join('&')
     return url + '?' + _encode;
@@ -23,27 +23,27 @@ class HttpRequest {
   }
   get(params) {
     return fetch(qs.url(this.url, params), this.options)
-      .then(function(res){
+      .then(function (res) {
         return res.json()
       })
   }
   post(params) {
     return fetch(this.url, Object.assign(this.options, {
-      methods: 'POST',
-      body: qs.stringify(params)
-    }))
-      .then(function(res){
+        methods: 'POST',
+        body: qs.stringify(params)
+      }))
+      .then(function (res) {
         return res.json()
       })
   }
 }
 
 
-const getPage = function(){
+const getPage = function () {
   const Req = new HttpRequest('http://fdfinger.github.io/json/page.json')
   return Req.get()
 }
-const getBlog = function(){
+const getBlog = function () {
   const Req = new HttpRequest('http://fdfinger.github.io/json/blog.json')
   return Req.get()
 }
